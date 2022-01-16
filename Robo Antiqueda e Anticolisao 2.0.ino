695d67c8be9a4ac2d795f4b4908b22d88496ee2a
coderHarrii <codeharry6@gmail.com>
2022-01-16 14:58:52 -0300

    Update Robo Antiqueda e Anticolisao 2.0.ino

--- Robo Antiqueda e Anticolisao 2.0.ino
+++ Robo Antiqueda e Anticolisao 2.0.ino
@@ -1,7 +1,7 @@
 /*____________________________________________________________________________________________________
 
-OBS: Esta é a versão mais atualizada que encontrei no pendrive, em breve providencio a versão final
-utilizada na apresentação da SNCT 2019 no IFSP de Sjc
+OBS: Esta é a versão mais atualizada que encontrei no pendrive, em breve providencio a versão
+utilizada na apresentação final
 _____________________________________________________________________________________________________*/
 
 #include <Ultrasonic.h>
